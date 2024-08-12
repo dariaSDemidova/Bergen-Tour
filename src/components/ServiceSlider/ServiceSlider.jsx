73de2ec34ebb4d6727cd4d74e25340c0a87d43ca
Mariia Mikhalchenko <m.mikhalchenko.de@gmail.com>
2024-08-12 15:56:39 +0200

    add service page

---
+++ src/components/ServiceSlider/ServiceSlider.jsx
@@ -0,0 +1,63 @@
+import { useState } from 'react';
+import PropTypes from 'prop-types';
+import './ServiceSlider.scss';
+
+const ServiceSlider = ({ images, arrowLeft, arrowRight }) => {
+  const [position, setPosition] = useState(1);
+
+  const slidesToScroll = 1;
+  const totalSlides = images.length;
+
+  const showSlides = (position) => {
+    return {
+      transform: `translateX(-${(position - 1) * 101}%)`
+    };
+  };
+
+  const handlePrevClick = () => {
+    setPosition(position === 1 ? totalSlides : position - slidesToScroll);
+  };
+
+  const handleNextClick = () => {
+    setPosition(position >= totalSlides ? 1 : position + slidesToScroll);
+  };
+
+  return (
+    <div className="service-slider">
+      <div className="service-slider__container">
+        <div
+          className="service-slider__items"
+          style={showSlides(position)}
+        >
+          {images.map((img, index) => (
+            <div className="service-slider__item" key={index}>
+              <img src={img} alt={`photo-${index + 1}`} />
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="service-slider__btns">
+        <button
+          className="service-slider__btn service-slider__btn_prev"
+          onClick={handlePrevClick}
+        >
+          <img src={arrowLeft} alt="prev" />
+        </button>
+        <button
+          className="service-slider__btn service-slider__btn_next"
+          onClick={handleNextClick}
+        >
+          <img src={arrowRight} alt="next" />
+        </button>
+      </div>
+    </div>
+  );
+};
+
+ServiceSlider.propTypes = {
+  images: PropTypes.arrayOf(PropTypes.string).isRequired,
+  arrowLeft: PropTypes.string.isRequired,
+  arrowRight: PropTypes.string.isRequired,
+};
+
+export default ServiceSlider;
--- src/pages/Service/Service.jsx
+++ src/pages/Service/Service.jsx
@@ -1,10 +1,43 @@
+import arrowLeft from '../../assets/icons/arrow-left.svg';
+import arrowRight from '../../assets/icons/arrow-right.svg';
+import pic1 from '../../assets/images/service-slider/pic1.jpeg';
+import pic2 from '../../assets/images/service-slider/pic2.jpeg';
+import pic3 from '../../assets/images/service-slider/pic3.jpeg';
+import pic4 from '../../assets/images/service-slider/pic4.jpeg';
+
+import ServiceSlider from '../../components/ServiceSlider/ServiceSlider';
 import './Service.scss';
 
 const Service = () => {
-    return (
-        <>
-        </>
-    );
+  const images = [pic1, pic2, pic3, pic4];
+
+  return (
+    <main>
+      <section className="service">
+        <div className="service-container">
+          <h1 className="service__header"><span className="service__header-orange">Мы предлагаем следующие виды услуг</span><br/>по
+            организации деловых и корпоративных<br/>мероприятий</h1>
+          <h2 className="service__subheader">различной сложности и уровня</h2>
+          <div className="service-wrapper">
+            <div className="service-block">
+              <ul className="service-block__list">
+                <li>Бронирование и продажа авиабилетов</li>
+                <li>Бронирование гостиниц</li>
+                <li>Организация конференций и аренда конференц-зала</li>
+                <li>Индивидуальные, групповые трансферы</li>
+                <li>VIP-обслуживание в залах аэропортов</li>
+                <li>Медицинское страхование выезжающих за рубеж</li>
+                <li>Организация поощрительных поездок для сотрудников и клиентов с возможностью предоставления «Туристского подарочного сертификата» любого номинала</li>
+                <li>Организация корпоративного путешествия на борту круизного корабля</li>
+                <li>Комплексная организация деловых визитов и отдыха во всех странах мира</li>
+              </ul>
+            </div>
+            <ServiceSlider images={images} arrowLeft={arrowLeft} arrowRight={arrowRight}/>
+          </div>
+        </div>
+      </section>
+    </main>
+  );
 };
 
 
