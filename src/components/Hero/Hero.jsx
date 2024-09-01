15038e4dc01ebe1c2a8d5508b12a424ae49fb5bc
Daria Demidova <daashenkaa@mail.ru>
2024-09-01 14:35:52 +0300

    add hero

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,14 @@
+import './Hero.scss';
+
+const Hero = () => {
+    return (
+        <section className="hero">
+            <div className="container">
+            <div className="hero__shedule">24/7</div>
+            <h1 className="hero__title">Мир слишком велик <span>чтобы сидеть на одном месте</span></h1>
+            </div>
+        </section>
+    );
+};
+
+export default Hero;
