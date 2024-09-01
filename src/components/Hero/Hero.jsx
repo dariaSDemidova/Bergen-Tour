9232751098718dbf5d9e8ee12b3a09160ae2fabe
Daria Demidova <daashenkaa@mail.ru>
2024-09-01 17:16:23 +0300

    add google form for popup and fix styles

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -4,8 +4,10 @@ const Hero = () => {
     return (
         <section className="hero">
             <div className="container">
-            <div className="hero__shedule">24/7</div>
-            <h1 className="hero__title">Мир слишком велик <span>чтобы сидеть на одном месте</span></h1>
+                <div className="hero__shedule">24/7</div>
+                <div className="hero__title-container">
+                    <h1 className="hero__title">Мир слишком велик <span>чтобы сидеть на одном месте</span></h1>
+                </div>
             </div>
         </section>
     );
